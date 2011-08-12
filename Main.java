456d3884777f147c52e0eb93f8e95e5400dc5a9c
Evan Chapman <iiechapman@gmail.com>
2011-08-11 22:03:11 -0400

    Pulled to desktop
    Checking to make sure everything works right, if it does, STOKED

--- Main.java
+++ Main.java
@@ -2,4 +2,5 @@ public static void main (String args[]){
 
 System.out.println("Hello World!");
 System.out.println("This is cool!");
+System.out.println("Welcome to the wonderful world of GIT!");
 }
